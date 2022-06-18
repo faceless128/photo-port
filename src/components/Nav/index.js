284c7c1d9a3f84a01fc450eea24dcb4ffeae7207
Kaijam Jeffers <faceless128@gmail.com>
2022-06-18 03:22:47 -0400

    link tweak

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -17,7 +17,7 @@ function Nav(props) {
    return (
     <header className="flex-row px-1">
       <h2>
-        <a data-testid="link" href="/">
+        <a data-testid="link" href="./">
           <span role="img" aria-label="camera"> {" "} 📸</span> {" "} Oh Snap!
         </a>
       </h2>
